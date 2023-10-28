3fb86fffa107af1542006ba5ff92b65c9e7e7e16
Leonardodev99 <pequenotiago2016@gmail.com>
2023-10-28 16:59:10 +0100

    Criando as classes Vehicle, CarRental, Invoice, RentalService e AngolaTaxService

---
+++ src/service/AngolaTaxService.java
@@ -0,0 +1,13 @@
+package service;
+
+public class AngolaTaxService {
+	public double tax(double amount) {
+		if(amount<=100.00) {
+			return amount*0.2;
+		}
+		else {
+			return amount*0.15;
+		}
+	}
+
+}
---
+++ src/service/RentalService.java
@@ -0,0 +1,61 @@
+package service;
+
+import entities.CarRental;
+
+public class RentalService {
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private AngolaTaxService taxService;
+
+	public RentalService() {
+		
+	}
+
+	public RentalService(Double pricePerHour, Double pricePerDay, AngolaTaxService taxService) {
+		
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+
+	public Double getPricePerHour() {
+		return pricePerHour;
+	}
+
+	public void setPricePerHour(Double pricePerHour) {
+		this.pricePerHour = pricePerHour;
+	}
+
+	public Double getPricePerDay() {
+		return pricePerDay;
+	}
+
+	public void setPricePerDay(Double pricePerDay) {
+		this.pricePerDay = pricePerDay;
+	}
+
+	public AngolaTaxService getTaxService() {
+		return taxService;
+	}
+
+	public void setTaxService(AngolaTaxService taxService) {
+		this.taxService = taxService;
+	}
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2= carRental.getFinish().getTime();
+		double duration = (double) (t2-t1)/1000/60/60;
+		int x = (int) Math.ceil(duration);
+		double basicPayment;
+		if(x<=12) {
+			basicPayment = pricePerHour*x;
+		}
+		else {
+			basicPayment = pricePerDay*x;
+		}
+		taxService.tax(basicPayment);
+	}
+	
+
+}
